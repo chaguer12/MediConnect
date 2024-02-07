68427dc094cb0909a2f822e496f2402f588c8d7f
zoolotoui <chag.med.amine@gmail.com>
2024-02-07 10:28:31 +0100

    core:building home page

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,5 @@
+@include('includes.head')
+<body class="bg-[#f3f4f6]">
+@include('includes.nav')
+
+</body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,157 @@
 @include('includes.head')
 <body class="bg-[#f3f4f6]">
     @include('includes.nav')
-   
-    
+ <section>
+ <div class="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
+  <div class="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
+    <svg class="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block" viewBox="0 0 100 100" fill="currentColor" preserveAspectRatio="none slice">
+      <path d="M50 0H100L50 100H0L50 0Z"></path>
+    </svg>
+    <img
+      class="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+      src="{{URL::asset('/images/gloves.jpg')}}"
+      alt=""
+    />
+  </div>
+  <div class="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
+    <div class="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
+      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+        Medi Connect
+      </p>
+      <h2 class="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+      Your journey to optimal health<br class="hidden md:block" />
+      Partnering for better health.
+        
+      </h2>
+      <p class="pr-5 mb-5 text-base text-gray-700 md:text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, reprehenderit? Eligendi dolor doloremque, blanditiis voluptatum fuga quo omnis amet non quod eveniet accusamus illo quidem alias quos at similique vitae!
+      </p>
+      <div class="flex items-center">
+        <a
+          href="/"
+          class="bg-[#4338ca] inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md hover:shadow-lg "
+        >
+          Get started
+        </a>
+        <a href="/" aria-label="" class="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700">Learn more</a>
+      </div>
+    </div>
+  </div>
+</div>
+ </section>
+ <section id="heads">
+ <h2 class="mb-8 py-16 text-center font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+ Find the answers you need
+      
+        
+</h2>
+    <!-- component -->
+<div class="container" style="margin: 0 auto;">
+          <div
+            class="flex flex-col md:grid grid-cols-9 mx-auto  text-blue-50">
+            <!-- left -->
+            <div class="flex flex-row-reverse md:contents">
+              <div
+                class="bg-[#4338ca] col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">Good doctors</h3>
+                <p class="leading-tight text-justify">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                  quaerat?
+                </p>
+              </div>
+              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#4338ca] shadow"
+                ></div>
+              </div>
+            </div>
+            <!-- right -->
+            <div class="flex md:contents">
+              <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#4338ca] shadow"
+                ></div>
+              </div>
+              <div
+                class="bg-[#4338ca] col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">Trust worthy</h3>
+                <p class="leading-tight text-justify">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Vitae, facilis.
+                </p>
+              </div>
+            </div>
+            <!-- left -->
+            <div class="flex flex-row-reverse md:contents">
+              <div
+                class="bg-[#4338ca] col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">Easy to use</h3>
+                <p class="leading-tight text-justify">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                  quaerat?
+                </p>
+              </div>
+              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#4338ca] shadow"
+                ></div>
+              </div>
+            </div>
+            <!-- left -->
+            <div class="flex flex-row-reverse md:contents">
+              <div
+                class="bg-[#4338ca] col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">flexible</h3>
+                <p class="leading-tight text-justify">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+                  quaerat?
+                </p>
+              </div>
+              <div class="col-start-5 col-end-6 md:mx-auto relative mr-10">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#4338ca] shadow"
+                ></div>
+              </div>
+            </div>
+            <!-- right -->
+            <div class="flex md:contents">
+              <div class="col-start-5 col-end-6 mr-10 md:mx-auto relative">
+                <div class="h-full w-6 flex items-center justify-center">
+                  <div class="h-full w-1 bg-blue-800 pointer-events-none"></div>
+                </div>
+                <div
+                  class="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-[#4338ca] shadow"
+                ></div>
+              </div>
+              <div
+                class="bg-[#4338ca] col-start-6 col-end-10 p-4 rounded-xl my-4 mr-auto shadow-md"
+              >
+                <h3 class="font-semibold text-lg mb-1">100% secure</h3>
+                <p class="leading-tight text-justify">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Vitae, facilis.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+ </section>
+@include('includes.footer')
 </body>
 </html>
